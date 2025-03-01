559f4e12e64e03740afc301eb82964c3676f11b6
Kommandor <a.tsymbaliuk2007@gmail.com>
2025-03-01 12:51:07 +0200

    add fasad

--- paterns/Program.cs
+++ paterns/Program.cs
@@ -6,51 +6,78 @@ using System.Threading.Tasks;
 
 namespace paterns
 {
-    public interface ICoffee
+    namespace FacadePattern
     {
-        string GetDescription();
-        double GetCost();
-    }
-    public class SimpleCoffee : ICoffee
-    {
-        public string GetDescription() => "Простий кавовий напій";
-        public double GetCost() => 10.0;
-    }
-    public abstract class CoffeeDecorator : ICoffee
-    {
-        protected ICoffee _coffee;
-
-        public CoffeeDecorator(ICoffee coffee)
+        public class Amplifier
         {
-            _coffee = coffee;
+            public void On() => Console.WriteLine("Amplifier is on.");
+            public void Off() => Console.WriteLine("Amplifier is off.");
+            public void SetVolume(int level) => Console.WriteLine($"Setting volume to {level}");
         }
-        public virtual string GetDescription() => _coffee.GetDescription();
-        public virtual double GetCost() => _coffee.GetCost();
-    }
-    public class MilkDecorator : CoffeeDecorator
-    {
-        public MilkDecorator(ICoffee coffee) : base(coffee) { }
-
-        public override string GetDescription() => _coffee.GetDescription() + ", з молоком";
-        public override double GetCost() => _coffee.GetCost() + 5.0;
-    }
-    public class SugarDecorator : CoffeeDecorator
-    {
-        public SugarDecorator(ICoffee coffee) : base(coffee) { }
-
-        public override string GetDescription() => _coffee.GetDescription() + ", з цукром";
-        public override double GetCost() => _coffee.GetCost() + 2.0;
-    }
-    class Program
-    {
-        static void Main()
+        public class Projector
         {
-            ICoffee coffee = new SimpleCoffee();
-            Console.WriteLine($"{coffee.GetDescription()} - {coffee.GetCost()} грн");
-            coffee = new MilkDecorator(coffee);
-            Console.WriteLine($"{coffee.GetDescription()} - {coffee.GetCost()} грн");
-            coffee = new SugarDecorator(coffee);
-            Console.WriteLine($"{coffee.GetDescription()} - {coffee.GetCost()} грн");
+            public void On() => Console.WriteLine("Projector is on.");
+            public void Off() => Console.WriteLine("Projector is off.");
+            public void SetInput(string input) => Console.WriteLine($"Setting input to {input}");
+        }
+        public class Screen
+        {
+            public void Down() => Console.WriteLine("Screen is down.");
+            public void Up() => Console.WriteLine("Screen is up.");
+        }
+        public class DVDPlayer
+        {
+            public void On() => Console.WriteLine("DVD player is on.");
+            public void Off() => Console.WriteLine("DVD player is off.");
+            public void Play(string movie) => Console.WriteLine($"Playing movie: {movie}");
+        }
+        public class HomeTheaterFacade
+        {
+            private Amplifier _amplifier;
+            private Projector _projector;
+            private Screen _screen;
+            private DVDPlayer _dvdPlayer;
+
+            public HomeTheaterFacade(Amplifier amplifier, Projector projector, Screen screen, DVDPlayer dvdPlayer)
+            {
+                _amplifier = amplifier;
+                _projector = projector;
+                _screen = screen;
+                _dvdPlayer = dvdPlayer;
+            }
+
+            public void WatchMovie(string movie)
+            {
+                Console.WriteLine("\nGet ready to watch a movie...");
+                _screen.Down();
+                _projector.On();
+                _projector.SetInput("DVD");
+                _amplifier.On();
+                _amplifier.SetVolume(5);
+                _dvdPlayer.On();
+                _dvdPlayer.Play(movie);
+            }
+            public void EndMovie()
+            {
+                Console.WriteLine("\nShutting movie theater down...");
+                _dvdPlayer.Off();
+                _amplifier.Off();
+                _projector.Off();
+                _screen.Up();
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Amplifier amplifier = new Amplifier();
+                Projector projector = new Projector();
+                Screen screen = new Screen();
+                DVDPlayer dvdPlayer = new DVDPlayer();
+                HomeTheaterFacade homeTheater = new HomeTheaterFacade(amplifier, projector, screen, dvdPlayer);
+                homeTheater.WatchMovie("The Matrix");
+                homeTheater.EndMovie();
+            }
         }
     }
 }
